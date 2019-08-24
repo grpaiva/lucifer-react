d0bd94aeee3ce15a13221e6f391e4726148e940a
grpaiva <gustavorpaiva@gmail.com>
2019-08-24 15:40:00 -0300

    cleans up App module

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  state = { displayBio: false };
-
-  toggleDisplayBio = () => {
-    this.setState({displayBio: !this.state.displayBio});
-  }
-
   render() {
     
     return (
